60139bad88c29cccb47506b8e8e5e60fffd9c14c
David <davidbalyan9@gmail.com>
2022-02-09 10:21:31 +0200

    Fixed background color

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import "./Timer.css";
 
 const Timer = () => {
+	const [bg, setBg] = useState({ backgroundColor: "#ff5a5f" });
+
 	const [start, setStart] = useState(0);
 	const [timeInput, setTimeInput] = useState(25);
 	const [time, setTime] = useState(timeInput * 60);
@@ -13,26 +15,47 @@ const Timer = () => {
 	const [bop, setBop] = useState(1);
 
 	useEffect(() => {
-		if (start && time > 0) {
+		if (start && time > -1) {
 			setTimeout(() => {
 				setTime(time - 1);
-				console.log(time);
 			}, 1000);
 
-			if (time < 2) {
+			if (time < 1) {
 				setBop(0);
+				setBg({ backgroundColor: "#087e8b" });
 				setRest(restInput * 60);
 				setStart(!start);
 			}
+
+			if (time === 3) {
+				setBg({ backgroundColor: "#ff2b32" });
+				setTimeout(() => {
+					setBg({ backgroundColor: "#ff5a5f" });
+				}, 500);
+			}
+
+			if (time === 2) {
+				setBg({ backgroundColor: "#ff2b32" });
+				setTimeout(() => {
+					setBg({ backgroundColor: "#ff5a5f" });
+				}, 500);
+			}
+
+			if (time === 1) {
+				setBg({ backgroundColor: "#ff2b32" });
+				setTimeout(() => {
+					setBg({ backgroundColor: "#ff5a5f" });
+				}, 500);
+			}
 		}
-		if (startRest && rest > 0) {
+		if (startRest && rest > -1) {
 			setTimeout(() => {
 				setRest(rest - 1);
-				console.log(rest);
 			}, 1000);
 
-			if (rest < 2) {
+			if (rest < 1) {
 				setBop(1);
+				setBg({ backgroundColor: "#ff5a5f" });
 				setTime(timeInput * 60);
 				setStartRest(!startRest);
 			}
@@ -40,28 +63,33 @@ const Timer = () => {
 	}, [time, start, startRest, rest, bop, restInput, timeInput]);
 
 	return (
-		<div className='timer-component'>
-			<div className='timer-and-button'>
+		<div className='timer-component' style={bg}>
+			<div className='timer'>
 				{bop ? (
-					<h2>
+					<h2 className='timer-h2'>
 						{Math.floor(time / 60)} : {Math.floor(time % 60)}
 					</h2>
 				) : (
-					<h2>
+					<h2 className='timer-h2'>
 						{Math.floor(rest / 60)} : {Math.floor(rest % 60)}
 					</h2>
 				)}
-
-				{bop ? (
-					<button onClick={() => setStart(!start)}>
-						{start ? "STOP" : "START"}
-					</button>
-				) : (
-					<button onClick={() => setStartRest(!startRest)}>
-						{startRest ? "STOP" : "START"}
-					</button>
-				)}
 			</div>
+			{bop ? (
+				<button
+					className='timer-button'
+					onClick={() => setStart(!start)}
+				>
+					{start ? "STOP" : "START"}
+				</button>
+			) : (
+				<button
+					className='timer-button'
+					onClick={() => setStartRest(!startRest)}
+				>
+					{startRest ? "STOP" : "START"}
+				</button>
+			)}
 
 			{/*bop ? <p>Pomodoro</p> : <p>Break</p>*/}
 
@@ -75,16 +103,21 @@ const Timer = () => {
 			>
 				<div className='input'>
 					<label htmlFor='time'>Time: </label>
+					<br />
 					<input
+						step='0.01'
 						type='number'
 						name='time'
+						className='input-1'
 						value={timeInput}
 						onChange={(e) => setTimeInput(e.target.value)}
 					/>
 				</div>
 				<div className='input'>
 					<label htmlFor='rest'>Break: </label>
+					<br />
 					<input
+						step='0.01'
 						type='number'
 						value={restInput}
 						onChange={(e) => setRestInput(e.target.value)}
